d371502896073a378c418a519d8b53f15377ce2c
Daud5 <nyanisodaud@gmail.com>
2023-06-18 18:27:57 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	int f;
+
+	for (f = 48; f < 58; f++)
+	{
+		putchar(f);
+	}
+	for (f = 97; f < 103; f++)
+	{
+		putchar(f);
+	}	
+        putchar('\n');
+	return (0);
+}		
